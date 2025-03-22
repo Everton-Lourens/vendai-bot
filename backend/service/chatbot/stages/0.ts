b7280001f9ece817f6569aa42ac77eab0667b87e
everton-lourens <evertonlourens@gmail.com>
2025-03-22 12:40:26 -0300

    - Adicionando banco de dados local usando JSON para ter maior controle das mensagens enviadas aos clientes && Adicionando histórico de respostas para saber o que o cliente escolheu e ter mais controle do que os clientes escolhem

--- backend/service/chatbot/stages/0.ts
+++ backend/service/chatbot/stages/0.ts
@@ -24,58 +24,5 @@ export const initialStage = {
       response
     };
 
-    /*
-    async function wellcome() {
-      storage[id].obj_store = {}; // zera caso o cliente deseje pedir outro atendente
-
-      // Recebe uma lista do nome das lojas
-      const listAllStore = await listOfStore({ from: id, numberChatbot: self });
-      // Caso tenha apenas 1 retorna apenas 1 objeto e não uma Array List
-      const itsJustOneStore = typeof listAllStore === 'object' && !Array.isArray(listAllStore.obj_store);
-      // É um objeto e não é um array (Só tem 1 Loja)
-      if (itsJustOneStore) {
-        return await oneStore();
-      } else {
-        return await manyStore();
-      }
-
-      async function oneStore() {
-        const lackOfPayment = await daysDifference(listAllStore.due_date);
-        // confere se pagou o bot se se não pagou desativa...
-        if (lackOfPayment || !listAllStore.chatbot_number) {
-          logger.error('CHATBOT DESATIVADO!' + ' - NUMERO DO CHATBOT::::: ' + self);
-          return 'CHATBOT DESATIVADO!';
-        }
-        // pega o único objeto store que foi cadastrada e coloca no storage do cliente
-        storage[id].obj_store = listAllStore;
-        // Como só tem uma Loja. retorna a mensagem do stage 1
-        const { message_1 } = msgStage[1];
-        // msg de bem-vindo + msg do stage 1 (fazer pedido) direto já que só tem 1 loja
-        return `Olá, *${client_name ?? 'tudo bem?'}*\n` + message_1;
-      }
-
-      async function manyStore() {
-        const lackOfPayment = await daysDifference(listAllStore.obj_store[0].due_date);
-        // confere se pagou o bot se se não pagou desativa...
-        if (lackOfPayment || !listAllStore.obj_store[0].chatbot_number) {
-          logger.error('CHATBOT DESATIVADO!' + ' - NUMERO DO CHATBOT::::: ' + self);
-          return 'CHATBOT DESATIVADO!';
-        }
-        const listNameStore = listAllStore?.listNameStore;
-        const obj_store = listAllStore?.obj_store;
-        // Armazena todas as Lojas no storage do cliente
-        storage[id].obj_store = obj_store; ///////////// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        // se tiver mais que 1 loja, retorna true e mostra Array List com os nomes das lojas
-        const { message_1 } = msgStage[0];
-        message_1 + '\n' + // Mensagem padrão
-          listNameStore; // nome das lojas e as opções para escolher uma loja
-        if (!listNameStore || !listNameStore?.length)
-          storage[id].attendant = true;
-        // Retorna as opções de loja que ele deseja ser atendido
-        return message_1 + '\n' + listNameStore;
-      }
-    }
-    */
-
   },
 }
