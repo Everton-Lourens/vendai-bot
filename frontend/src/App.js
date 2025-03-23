1039e07030025995fa54b164bdb85cb7390b2495
everton-lourens <evertonlourens@gmail.com>
2025-03-22 23:38:54 -0300

    Atualizando css do chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,32 +5,64 @@ const App = () => {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const messageListRef = useRef(null);
-  
+  const [messageResponse, setMessageResponse] = useState('');
+
+  const [dataResponseChatbot, setDataResponseChatbot] = useState({
+    client: {
+      id: 999,
+      stage: 0,
+      message: '',
+    }
+  });
+
   useEffect(() => {
     if (messageListRef.current) {
       messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
     }
   }, [messages]); // Sempre que `chatMessages` mudar, o chat rola para baixo
 
-  const handleSendMessage = () => {
+  const sendMessageToChatbot = () => {
     if (input.trim()) {
       setMessages([
         ...messages,
         { text: input, sender: 'user' },
-        { text: 'Pensando...', sender: 'bot' },
       ]);
-      setInput('');
-      
-      // Simulando a resposta do bot após 1 segundo
+      apiChatbot();
       setTimeout(() => {
-        setMessages((prevMessages) => {
-          const updatedMessages = [...prevMessages];
-          updatedMessages[updatedMessages.length - 1] = { text: 'Resposta do bot', sender: 'bot' };
-          return updatedMessages;
-        });
+        sendMessageFromChatbot();
       }, 1000);
+      setInput('');
     }
   };
+
+  const apiChatbot = async () => {
+    fetch('http://localhost:3005/v1/chat', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dataResponseChatbot),
+    }).then((response) => response.json())
+      .then((data) => {
+        const { client } = data?.data;
+        setDataResponseChatbot({ client });
+        setMessageResponse(data?.data?.client?.response);
+      })
+      .catch((error) => {
+        console.error('Erro ao enviar mensagem:', error);
+      });
+  };
+
+
+  const sendMessageFromChatbot = React.useCallback(() => {
+    !messageResponse && apiChatbot();
+    messageResponse && setMessages((prevMessages) => [...prevMessages, { text: messageResponse, sender: "bot" }]);
+  }, [messageResponse]);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      sendMessageFromChatbot();
+    }, 500);
+  }, [messageResponse, sendMessageFromChatbot]);
 
   return (
     <div className="chat-container">
@@ -49,9 +81,10 @@ const App = () => {
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && sendMessageToChatbot()}
           placeholder="Digite sua mensagem..."
         />
-        <button onClick={handleSendMessage}>Enviar</button>
+        <button onClick={sendMessageToChatbot}>Enviar</button>
       </div>
     </div>
   );
