5f61386bfffb3b5929ba5ead05eae4f013238d9c
everton-lourens <evertonlourens@gmail.com>
2025-05-06 12:32:38 -0300

    Corrigindo dependência do docker que estava dando erro nas conexões do banco de dados

---
+++ frontend/next.config.js
@@ -0,0 +1,18 @@
+const nextConfig = {
+  reactStrictMode: true,
+  // Garantir que o Next.js escute em todos os IPs
+  server: {
+    host: '0.0.0.0', // Ouvindo em todos os IPs
+  },
+  webpack(config, { isServer }) {
+    // Permite a configuração do Webpack no Next.js
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // Evita erro de dependências do Next.js ao rodar no Docker
+      }
+    }
+    return config
+  },
+}
+
+export default nextConfig
