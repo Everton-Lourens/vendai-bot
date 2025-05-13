3c19c46ae51a209c4f31138e67c85275d0527483
everton-lourens <evertonlourens@gmail.com>
2025-05-13 13:19:02 -0300

    Configurando pool do mongo

--- backend/src/database/mongo/mongoConfigs.ts
+++ backend/src/database/mongo/mongoConfigs.ts
@@ -2,16 +2,17 @@ import mongoose from 'mongoose'
 
 const MONGO_USERNAME = 'lourens';
 const MONGO_PASSWORD = 'u72E1K8bxPcVgFhM';
-const mongoURL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.5klkiuy.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURL =  process.env.DB_URL || `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.5klkiuy.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoURL)
+mongoose.connect(mongoURL, {
+  maxPoolSize: Number(process.env.DB_POOL) || 100,
+  serverSelectionTimeoutMS: 10000,
+});
+
 mongoose.connection
-  .on(
-    'error',
-    console.error.bind(console, 'Erro ao conectar com o banco de dados'),
-  )
+  .on('error', console.error.bind(console, 'Erro ao conectar com o banco de dados'))
   .once('open', () => {
-    console.log('Conexão com o banco de dados (mongo) estabelecida com sucesso')
-  })
+    console.log('Conexão com o banco de dados (mongo) estabelecida com sucesso');
+  });
 
 export default mongoose
