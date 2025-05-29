ab3c2a7935c23e48211920de71040138b9c48386
everton-lourens <evertonlourens@gmail.com>
2025-05-29 14:29:30 -0300

    feat: Iniciando criação do hook para comunicação com o chatbot

--- frontend/src/components/screens/Chatbot/hooks/useFormMessage.tsx
+++ frontend/src/components/screens/Chatbot/hooks/useFormMessage.tsx
@@ -38,6 +38,36 @@ export function useFormMessage({ handleClose, messageDataToEdit }: Props) {
   const router = useRouter()
   const [anchorEl, setAnchorEl] = useState<Element | null>(null)
 
+  function onResposeChatbot(message: INewMessage) {
+    messageService
+      .create({ ...newMessage }, httpClientProvider)
+      .then(() => {
+        router.push({
+          pathname: router.route,
+          query: router.query,
+        })
+
+        reset()
+
+        handleClose()
+
+        setAlertNotifyConfigs({
+          ...alertNotifyConfigs,
+          open: true,
+          type: ALERT_NOTIFY_TYPE.SUCCESS,
+          text: 'Mensagem cadastrada com sucesso',
+        })
+      })
+      .catch((err) => {
+        setAlertNotifyConfigs({
+          ...alertNotifyConfigs,
+          open: true,
+          type: ALERT_NOTIFY_TYPE.ERROR,
+          text: `Erro ao tentar cadastrar mensagem - ${err?.message}`,
+        })
+      })
+  }
+
   function onCreateNewMessage(newMessage: INewMessage) {
     messageService
       .create({ ...newMessage }, httpClientProvider)
