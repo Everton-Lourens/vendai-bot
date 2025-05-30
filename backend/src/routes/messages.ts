d8d2bb8e7501d250c0eae9922e39ac8fa2e915e3
everton-lourens <evertonlourens@gmail.com>
2025-05-30 09:17:42 -0300

    refactor: retirando balidação de json do chatbot

--- backend/src/routes/messages.ts
+++ backend/src/routes/messages.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import { MessageController } from '../controllers/MessageController'
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
-import { validationFilter } from '../middlewares/middleware'
 
 const mensagemRoutes = express.Router()
 const messageController = new MessageController()
@@ -11,8 +10,6 @@ mensagemRoutes.use(ensureAuthenticated)
 
 // Routes
 mensagemRoutes.get('/', messageController.listMessages.bind(messageController))
-mensagemRoutes.post('/chat', validationFilter, messageController.sendMessageToChatbot.bind(messageController))
-mensagemRoutes.get('/chat', messageController.sendMessageToChatbot.bind(messageController))
 mensagemRoutes.get('/padroes', messageController.getDefaultMessages.bind(messageController))
 mensagemRoutes.post('/', messageController.createNewMessage.bind(messageController))
 mensagemRoutes.put('/', messageController.updateMessage.bind(messageController))
