1340741637f03d031b28bd0e105c70ddc1f2e004
everton-lourens <evertonlourens@gmail.com>
2025-06-10 14:39:54 -0300

    refactor: mudando mensagem do chatbot

--- backend/src/config/defaultMessages/defaultMessages.ts
+++ backend/src/config/defaultMessages/defaultMessages.ts
@@ -1,18 +1,13 @@
 export default [
   {
-    stage: 4,
-    position: 1,
-    text: 'TEXTO 4, POSIÇÃO 1',
-  },
-  {
     stage: 3,
     position: 1,
-    text: '(Stage atendente) Aguarde um momento, você será atendido por um atendente humano. 🧑‍💼',
+    text: 'Aguarde um momento, você será atendido por um atendente humano. 🧑‍💼',
   },
   {
     stage: 2,
     position: 1,
-    text: 'TEXTO 2, POSIÇÃO 1',
+    text: 'Confira os produtos disponíveis:\n',
   },
   {
     stage: 1,
